14c59aa39ef77f894b40dd288c3d5c448ee64ff8
kuuuube <hexagonisalie@gmail.com>
2023-07-30 12:08:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# bigdecimal-rs-pow
+
+Code snippets for power and root operations on bigdecimal types.
+
+## Disclaimer
+
+### Only use this code if you need something that works **NOT** something highly performant or "bulletproof"
+
+I do not understand the math this is doing and cannot vouch for its efficiency in any way. Portions of the algorithm I came up with myself I also cannot guarantee will function efficiently or performantly.
+
+There may be unforseen ways this code could break.
+
+## Usage
+
+Take a look at the [source](./src/main.rs). It should be relatively easy to understand how to use.
+
+If you are handling negative powers or roots you will need to add your own handling for them. My code only supports positive numbers (including zero).
+
+## License
+
+This code is dual-licensed under the permissive [MIT](https://opensource.org/licenses/MIT) & [Apache 2.0](https://opensource.org/licenses/Apache-2.0) licenses.
+
+This is the same as bigdecimal-rs.
+
+Any project using bigdecimal-rs may also use this code.
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use bigdecimal::{BigDecimal, Zero, FromPrimitive};
+use std::str::FromStr;
+
+fn main() {
+    //1.23456789^2.55  (round 50 before starting calculations, precision 50 in the root loop)
+    println!("{}", bigdecimal_powf(&BigDecimal::from_str("1.23456789").unwrap(), &BigDecimal::from_str("2.55").unwrap(), Some(50), Some(50)));
+    //123^2
+    println!("{}", bigdecimal_powi(&BigDecimal::from_str("123").unwrap(), &BigDecimal::from_str("2").unwrap()));
+    //322nd root of 2  (precision 50 in the root loop)
+    println!("{}", bigdecimal_root(&BigDecimal::from_str("322").unwrap(), &BigDecimal::from_str("2").unwrap(), Some(50)));
+}
+
+//calculates integer and decimal bigdecimal powers powers
+//be warned that passing none to prec may cause your calculations to take forever
+//x is the base, e is the exponent, prec tries to remove effectively infinite calculations, round rounds the inputs of powi to be faster
+pub fn bigdecimal_powf(x: &BigDecimal, e: &BigDecimal, prec: Option<u64>, round: Option<i64>) -> BigDecimal {
+    let exponent_string = format!("{}", e);
+    let split: Vec<&str> = exponent_string.split(".").collect();
+    let split_whole = split.get(0).unwrap();
+    let split_decimal = split.get(1).unwrap_or(&"");
+
+    let whole_value = BigDecimal::from_str(split_whole).unwrap();
+
+    if split_decimal.len() >= 1 {
+        let numerator = split_whole.parse::<u32>().unwrap() * u32::pow(10, split_decimal.len() as u32) + split_decimal.parse::<u32>().unwrap();
+        let denominator = u32::pow(10, split_decimal.len() as u32);
+        
+        //attempt to chop down the fraction to improve performance if a greatest common denominator can be found
+        let gcd = euclid_gcd(numerator, denominator);
+        let simplified_numerator = numerator / gcd;
+        let simplified_denominator = denominator / gcd;
+
+        let whole_result = if round.is_some() {
+            bigdecimal_powi(&x.round(round.unwrap()), &BigDecimal::from_u32(simplified_numerator).unwrap()).round(round.unwrap())
+        } else {
+            bigdecimal_powi(&x, &BigDecimal::from_u32(simplified_numerator).unwrap())
+        };
+        return bigdecimal_root(&BigDecimal::from_u32(simplified_denominator).unwrap(), &whole_result, prec);
+    } else {
+        return bigdecimal_powi(&x, &whole_value);
+    }
+}
+
+//simple greatest common denominator finder
+//m is the numerator, n is the denominator
+fn euclid_gcd(mut m: u32, mut n: u32) -> u32 {
+    while m != 0 {
+        let old_m = m;
+        m = n % m;
+        n = old_m;
+    }
+    return n
+ }
+
+//calculates integer equivalent bigdecimal powers only
+//x is the base, e is the exponent
+pub fn bigdecimal_powi(x: &BigDecimal, e: &BigDecimal) -> BigDecimal {
+    let mut r = BigDecimal::from_str("1").unwrap();
+    let mut i = BigDecimal::zero();
+    while i < *e {
+        r *= x;
+        i += 1;
+    }
+    return r;
+}
+
+//calculates integer equivalent bigdecimal roots only
+//be warned that passing none to prec may cause your calculations to take forever
+//n is the root, x is the base, prec tries to remove effectively infinite calculations
+pub fn bigdecimal_root(n: &BigDecimal, x: &BigDecimal, prec: Option<u64>) -> BigDecimal {
+    let mut d: BigDecimal;
+    let mut r = BigDecimal::from_str("1").unwrap();
+    if x == &BigDecimal::zero() {
+        return BigDecimal::zero();
+    }
+    if n < &BigDecimal::from_str("1").unwrap() { //this if was `if (n < 1 || (x < 0 && !(n&1)))` in C, depending on the use case you may need to find a way to add the rest
+        return BigDecimal::zero(); //substitute for NaN, you may want to convert this function to return an option if you need to handle this case
+    }
+    loop {
+        r = if prec.is_some() {r.with_prec(prec.unwrap())} else {r};  //looping with round is too expensive, with_prec is used instead
+        d = (x / bigdecimal_powi(&r, &(n - 1)) - &r) / n;
+        r += &d;
+        if !(&d >= &(BigDecimal::from_f64(f64::EPSILON).unwrap() * 10) || &d <= &(BigDecimal::from_f64(-f64::EPSILON).unwrap() * 10)) {
+            break;
+        }
+    }
+    return r;
+}
